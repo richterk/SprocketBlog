5130db6b6bb7cd13e7f49f0ba49317074f274fb0
Kyle Richter <kyle.richter@fallenjusticegames.com>
2012-07-10 09:49:19 -0500

    Initial commit

---
+++ SprocketBlog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SprocketBlog::Application.config.secret_token = '2b0ba1bdd65a722d8bd6ccdc2058887ee1086651ef0d48f42b89c961cd1b2f4886b844529ce3da1598231815941d4a8a09187932109cf418305489610604e45f'
